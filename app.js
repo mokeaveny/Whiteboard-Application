69c75819c07af1b83072404b29cc4e5c7ff319e8
M Keaveny <331-mk642@users.noreply.campus.cs.le.ac.uk>
2022-11-14 18:05:01 +0000

    Implemented code that utilising the canvas HTML element we are able to draw lines

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 let canvas = document.getElementById("canvas");
 
-canvas.width = window.innerWidth;
+canvas.width = 0.98 * window.innerWidth;
 canvas.height = window.innerHeight;
 
 let canvasContext = canvas.getContext("2d");
+
+let x;
+let y;
+let mouseClicked = false;
+
+window.onmousedown = (e) => {
+    canvasContext.moveTo(x, y);
+    mouseClicked = true;
+}
+
+window.onmouseup = (e) => {
+    mouseClicked = false;
+}
+
+window.onmousemove = (e) => {
+    x = e.clientX;
+    y = e.clientY;
+
+    if (mouseClicked == true)
+    {
+        canvasContext.lineTo(x,y);
+        canvasContext.stroke();
+    }
+};
